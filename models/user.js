b1f32a77e03bba105b6c7940654c81ab5dca09ab
Robbie Gill <gill.rob@gmail.com>
2013-04-23 07:54:58 -0700

    use passport-glue

--- models/user.js
+++ models/user.js
@@ -1,14 +1,18 @@
 var mongoose = require('mongoose')
-  , users = require('passport-users')
+  , users = require('passport-glue')
   , userSchema = users.userSchema
   , twitter_plugin = users.twitter_oauth1a_plugin
   , google_plugin = users.google_oauth2_plugin
   , github_plugin = users.github_oauth2_plugin
-  , sessions_plugin = users.persistent_sessions_plugin;
+  , sessions_plugin = users.persistent_sessions_plugin
+  , ts_created = require('goose-plugins').ts_created
+  , ts_modified = require('goose-plugins').ts_modified;
 
 userSchema.plugin(twitter_plugin);
 userSchema.plugin(github_plugin);
 userSchema.plugin(google_plugin);
 userSchema.plugin(sessions_plugin);
+userSchema.plugin(ts_created);
+userSchema.plugin(ts_modified);
 
 module.exports = User = mongoose.model('User', userSchema);
